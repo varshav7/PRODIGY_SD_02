24da27b4dde81a828ac7770ad4ec7c415b399c03
varshav7 <varshav.3007@gmail.com>
2024-08-07 19:50:17 +0530

    Update prgm2.cpp

--- prgm2.cpp
+++ prgm2.cpp
@@ -1,26 +1,36 @@
-#include<stdio.h>
-#include<conio.h>
-#include<stdlib.h>
-#include<time.h>
-int main()
-{
-	int original,guess,ctr=0;
-	srand(time(0));
-	original=rand();
-	label1:
-	printf("\nEnter your guess:\t");
-	scanf("%d",&guess);
-	if(guess!=original)
-	{
-		printf("wrong,try again\n");
-		if(original>guess)
-		printf("The number is higher than the guess");
-		else
-		printf("The number is lower than the guess");
-		ctr++;
-		goto label1;
-	}
-	else
-	printf("\nGuess is right, you took %d tries to get the answer",ctr+1);
-	
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    int target_number, guess, attempts = 0;
+    char input[10];
+    srand(time(0));
+    target_number = rand() % 100 + 1;
+
+    printf("Guess the number between 1 and 100:\n");
+
+    while (1) {
+        printf("Enter your guess: (Enter -1 to quit)");
+        scanf("%d",&guess);
+        if (guess== -1) {
+            printf("You chose to quit. The number was: %d\n", target_number);
+            break;
+        }
+
+        attempts++;
+
+        // Compare guess to target number and provide feedback
+        if (guess < target_number) {
+            printf("Too low! Try again.\n");
+        } else if (guess > target_number) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the number in %d attempts.\n", attempts);
+            break;
+        }
+    }
+
+    return 0;
 }
+
